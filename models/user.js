d0bfc9565ff2ed7d61e79fd92614304fb2ee1d6b
Mateo Álvarez <alvarez.fing@gmail.com>
2021-10-26 23:35:27 -0300

    model of users

---
+++ models/user.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose')
+
+const UserSchema = Schema({
+  displayName: {
+    type: String,
+    required: [true, 'displayName is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'email is required'],
+  },
+  password: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+    default: null,
+  },
+  role: {
+    type: String,
+    required: [true, 'role is required'],
+    enum: ['SPORTPLAYER', 'TRAINER'],
+  },
+  code: {
+    type: String,
+    default: null,
+  },
+  state: {
+    type: Boolean,
+    default: true,
+  },
+  google: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+UserSchema.methods.toJSON = function () {
+  const { __v, password, _id: uid, ...restInfo } = this.toObject()
+  return {
+    ...restInfo,
+    uid,
+  }
+}
+
+module.exports = model('User', UserSchema)
