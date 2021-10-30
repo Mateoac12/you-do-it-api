209606a83e6349d5bf917a3101722a4d1dbfc480
Mateo Álvarez <alvarez.fing@gmail.com>
2021-10-29 23:28:01 -0300

    route of classes

---
+++ routes/classes.routes.js
@@ -0,0 +1,42 @@
+const { Router } = require('express')
+const { body } = require('express-validator')
+const {
+  createClass,
+  addSportPlayer,
+} = require('../controllers/classes.controller')
+const {
+  validatorErrors,
+  validateToken,
+  trainerValidator,
+  sportPlayerValidator,
+} = require('../middlewares')
+
+const router = Router()
+
+router.post(
+  '/add',
+  [
+    validateToken,
+    trainerValidator,
+    body('creator', 'Is required a correct ID').isMongoId(),
+    body('activity', 'Activity is required').notEmpty(),
+    body('limitQuotas', 'Required to be a number').isNumeric().notEmpty(),
+    body('date', 'Required a date').isNumeric().notEmpty(),
+    validatorErrors,
+  ],
+  createClass
+)
+
+router.put(
+  '/',
+  [
+    validateToken,
+    sportPlayerValidator,
+    body('uid', 'User ID is required').isMongoId(),
+    body('sportClassID', 'Is required a class ID').isMongoId(),
+    validatorErrors,
+  ],
+  addSportPlayer
+)
+
+module.exports = router
