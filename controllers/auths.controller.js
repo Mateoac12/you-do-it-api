889bf23230e1c559c348e61d9d784adb615963ca
Mateo Álvarez <alvarez.fing@gmail.com>
2021-10-29 23:25:21 -0300

    auth of controllers

---
+++ controllers/auths.controller.js
@@ -0,0 +1,41 @@
+const bcryptjs = require('bcryptjs')
+const { jwtGenerator } = require('../helpers/jwtGenerator')
+const User = require('../models/user')
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body
+
+  const user = await User.findOne({ email })
+
+  if (!user || !user?.state) {
+    return res.status(400).json({
+      error: 'User not exist',
+    })
+  }
+
+  const { password: passwordHash } = user
+  const isSamePassword = bcryptjs.compareSync(password, passwordHash)
+
+  if (!isSamePassword) {
+    return res.status(400).json({
+      error: 'Incorrect password or login with signIn with Google',
+    })
+  }
+
+  try {
+    const tokenID = await jwtGenerator({ uid: user.id })
+    res.json({
+      user,
+      tokenID,
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      error,
+    })
+  }
+}
+
+module.exports = {
+  loginUser,
+}
