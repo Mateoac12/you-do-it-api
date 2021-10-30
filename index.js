49b85271d206dc87b3377f67821b272a1479e1b5
Mateo Álvarez <alvarez.fing@gmail.com>
2021-10-29 23:23:20 -0300

    add routes and middleware of files

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const cors = require('cors')
 const express = require('express')
+const fileUpload = require('express-fileupload')
 
 const app = express()
 
@@ -13,9 +14,18 @@ const PORT = process.env.PORT || 8080
 // middlewares
 app.use(cors())
 app.use(express.json())
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: '/tmp/',
+  })
+)
 
 // routes
 app.use('/users', require('./routes/users.routes'))
+app.use('/auth', require('./routes/auths.routes'))
+app.use('/classes', require('./routes/classes.routes'))
+app.use('/search', require('./routes/searches.routes'))
 
 app.listen(PORT, () => {
   console.log(`Connecting from http://localhost:${PORT}`)
