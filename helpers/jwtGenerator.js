e0114d030abaee36444c901b0f803085d358e6c8
Mateo Álvarez <alvarez.fing@gmail.com>
2021-11-03 19:26:28 -0300

    remove expirer validator

--- helpers/jwtGenerator.js
+++ helpers/jwtGenerator.js
@@ -2,17 +2,10 @@ const jwt = require('jsonwebtoken')
 
 const jwtGenerator = async (data) => {
   return new Promise((resolve, reject) => {
-    jwt.sign(
-      data,
-      process.env.JWT_KEY,
-      {
-        expiresIn: '4h',
-      },
-      (err, token) => {
-        if (err) reject(err.message)
-        resolve(token)
-      }
-    )
+    jwt.sign(data, process.env.JWT_KEY, (err, token) => {
+      if (err) reject(err.message)
+      resolve(token)
+    })
   })
 }
 
